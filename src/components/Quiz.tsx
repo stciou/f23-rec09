29070e01513070c796445671a739df462f671d9e
Shang-Ting Ciou <sciou@andrew.cmu.edu>
2023-11-01 11:52:49 -0400

    Updated code

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,78 +1,55 @@
-import React, { Component, useState } from 'react'
-import './Quiz.css'
-import QuizQuestion from '../core/QuizQuestion';
+import React, { useState, useEffect } from 'react';
+import './Quiz.css';
 import QuizCore from '../core/QuizCore';
 
-interface QuizState {
-  questions: QuizQuestion[];
-  currentQuestionIndex: number;
-  selectedAnswer: string | null;
-  score: number;
-  quizCompleted: boolean;
-}
-
 const Quiz: React.FC = () => {
-  const initialQuestions: QuizQuestion[] = [
-    {
-      question: 'What is the capital of France?',
-      options: ['London', 'Berlin', 'Paris', 'Madrid'],
-      correctAnswer: 'Paris',
-    },
-  ];
-  
-  const [state, setState] = useState<QuizState>({
-    questions: [],
-    currentQuestionIndex: 0,
-    selectedAnswer: null,
-    score: 0,
-    quizCompleted: false,
-  });
-
   const quizCore = new QuizCore();
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+  const [score, setScore] = useState<number>(0);
+  const [currentQuestion, setCurrentQuestion] = useState(quizCore.getCurrentQuestion());
+  const [quizFinished, setQuizFinished] = useState(false);
 
-  const currentQuestion = quizCore.getCurrentQuestion();
-
-const handleOptionSelect = (option: string): void => {
-  quizCore.answerQuestion(option);
-  setState((prevState) => ({ ...prevState, selectedAnswer: option }));
-};
-
-const handleButtonClick = (): void => {
-  if (quizCore.hasNextQuestion()) {
-    quizCore.nextQuestion();
-    setState((prevState) => ({ ...prevState, currentQuestionIndex: prevState.currentQuestionIndex + 1, selectedAnswer: null }));
-  } else {
-    setState((prevState) => ({ ...prevState, quizCompleted: true, score: quizCore.getScore() }));
+  const handleOptionSelect = (option: string): void => {
+    setSelectedAnswer(option);
   }
-};
 
+  const handleNextQuestion = (): void => {
+    if (selectedAnswer) {
+      quizCore.answerQuestion(selectedAnswer);
+      setScore(quizCore.getScore());
+    }
+    
+    if (quizCore.hasNextQuestion()) {
+      quizCore.nextQuestion();
+      setCurrentQuestion(quizCore.getCurrentQuestion());
+      setSelectedAnswer(null);
+    } else {
+      setQuizFinished(true);
+    }
+  }
 
-  const { questions, currentQuestionIndex, selectedAnswer, score } = state;
-  // const currentQuestion = questions[currentQuestionIndex];
+  useEffect(() => {
+    setCurrentQuestion(quizCore.getCurrentQuestion());
+  }, [quizCore]);
+
+  if (quizFinished) {
+    return (
+      <div>
+        <h2>Quiz Completed</h2>
+        <p>Final Score: {score} out of {quizCore.getTotalQuestions()}</p>
+      </div>
+    );
+  }
 
   if (!currentQuestion) {
-    return (
-      <div>
-        <h2>Quiz Completed</h2>
-        <p>Final Score: {score} out of {questions.length}</p>
-      </div>
-    );
-  }
-
-  if (state.quizCompleted) {
-    return (
-      <div>
-        <h2>Quiz Completed</h2>
-        <p>Final Score: {state.score} out of {quizCore.getQuestions().length}</p>
-      </div>
-    );
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
       <h2>Quiz Question:</h2>
       <p>{currentQuestion.question}</p>
-    
+
       <h3>Answer Options:</h3>
       <ul>
         {currentQuestion.options.map((option) => (
@@ -89,9 +66,8 @@ const handleButtonClick = (): void => {
       <h3>Selected Answer:</h3>
       <p>{selectedAnswer ?? 'No answer selected'}</p>
 
-      <button onClick={handleButtonClick}>Next Question</button>
+      <button onClick={handleNextQuestion}>Next Question</button>
     </div>
-    
   );
 };
 
