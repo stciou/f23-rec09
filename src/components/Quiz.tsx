8b0b0e6f3a38e6d026196c95ce32dd2bcacc0bd4
Shang-Ting Ciou <sciou@andrew.cmu.edu>
2023-11-01 12:17:43 -0400

    Fixed

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,63 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import './Quiz.css';
+import React, { useState, useEffect } from 'react'
+import './Quiz.css'
 import QuizCore from '../core/QuizCore';
 
 const Quiz: React.FC = () => {
   const [quizCore] = useState(new QuizCore());
-  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
-  const [score, setScore] = useState<number>(0);
   const [currentQuestion, setCurrentQuestion] = useState(quizCore.getCurrentQuestion());
-  const [quizFinished, setQuizFinished] = useState(false);
+  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    setCurrentQuestion(quizCore.getCurrentQuestion());
+  }, [quizCore]);
 
   const handleOptionSelect = (option: string): void => {
     setSelectedAnswer(option);
   }
 
-  const handleSubmit = (): void => {
-    if (selectedAnswer) {
+  const handleButtonClick = (): void => {
+    if (selectedAnswer !== null) {
       quizCore.answerQuestion(selectedAnswer);
-      setScore(quizCore.getScore());
-    }
-    setQuizFinished(true);
-  }
-
-  const handleNextQuestion = (): void => {
-    if (selectedAnswer) {
-      quizCore.answerQuestion(selectedAnswer);
-      setScore(quizCore.getScore());
-      setSelectedAnswer(null);
-    }
-
-    if (quizCore.hasNextQuestion()) {
-      quizCore.nextQuestion();
-      setCurrentQuestion(quizCore.getCurrentQuestion());
-    } else {
-      handleSubmit();
+      if (quizCore.hasNextQuestion()) {
+        quizCore.nextQuestion();
+        setCurrentQuestion(quizCore.getCurrentQuestion());
+        setSelectedAnswer(null);
+      } else {
+        setScore(quizCore.getScore());
+      }
     }
   }
 
-  useEffect(() => {
-    setCurrentQuestion(quizCore.getCurrentQuestion());
-  }, [quizCore, quizCore.getCurrentQuestionIndex()]);
-
-  if (quizFinished) {
+  if (currentQuestion === null) {
     return (
       <div>
         <h2>Quiz Completed</h2>
-        <p>Final Score: {score} out of {quizCore.getTotalQuestions()}</p>
+        <p>Final Score: {score}</p>
       </div>
     );
-  }
-
-  if (!currentQuestion) {
-    return <div>Loading...</div>;
   }
 
   return (
     <div>
       <h2>Quiz Question:</h2>
       <p>{currentQuestion.question}</p>
-
+    
       <h3>Answer Options:</h3>
       <ul>
         {currentQuestion.options.map((option) => (
@@ -70,15 +55,14 @@ const Quiz: React.FC = () => {
           </li>
         ))}
       </ul>
-
-      <h3>Selected Answer:</h3>
-      <p>{selectedAnswer ?? 'No answer selected'}</p>
-
-      <button onClick={handleNextQuestion}>
-        {quizCore.hasNextQuestion() ? 'Next Question' : 'Submit'}
-      </button>
+  
+      {quizCore.hasNextQuestion() ? (
+        <button onClick={handleButtonClick}>Next Question</button>
+      ) : (
+        <button onClick={handleButtonClick}>Submit</button>
+      )}
     </div>
-  );
+  );  
 };
 
 export default Quiz;
